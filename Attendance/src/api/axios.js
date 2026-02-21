93b7ff758ba912725805885d5d5f8891bfdadf87
Sunil kannaujiya <skannaujiya920@gmail.com>
2026-02-21 23:37:21 +0530

    Change API base URL to production endpoint

--- Attendance/src/api/axios.js
+++ Attendance/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://mini-attendence.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
