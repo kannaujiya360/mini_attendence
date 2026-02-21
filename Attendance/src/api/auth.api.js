086cd7a2d768747a7469b90e028c8aa7f8043be0
Sunil kannaujiya <skannaujiya920@gmail.com>
2026-02-22 00:33:17 +0530

    Update auth.api.js

--- Attendance/src/api/auth.api.js
+++ Attendance/src/api/auth.api.js
@@ -1,11 +1,11 @@
 import API from "./axios";
 
 export const registerUser = async (data) => {
-  const response = await API.post("/auth/register", data);
+  const response = await API.post("/api/auth/register", data);
   return response.data; 
 };
 
 export const loginUser = async (data) => {
-  const response = await API.post("/auth/login", data);
+  const response = await API.post("/api/auth/login", data);
   return response.data;
 };
